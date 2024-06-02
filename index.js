81ee89162caca4f4ac5a0c0ae7d5836bb854f935
devsafi <mdkawserferdoussafi@gmail.com>
2024-06-02 14:58:55 +0600

    api create for users

--- index.js
+++ index.js
@@ -29,13 +29,30 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+
+        const userCollection = client.db("contestDb").collection("users")
+
+        // user collection is here
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // const query = { email: user.email };
+            // const existingUser = await userCollection.findOne(query);
+            // if (existingUser) {
+            //     return res.send({ message: "User Already Exists", insertId: null })
+            // }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        // contest collection is here
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
