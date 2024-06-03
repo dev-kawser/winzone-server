44fbe151460de01b133a356abcde0d90845f78d7
devsafi <mdkawserferdoussafi@gmail.com>
2024-06-03 11:00:00 +0600

    api create for get contest

--- index.js
+++ index.js
@@ -132,6 +132,12 @@ async function run() {
             res.send(result);
         })
 
+        // get contes
+        app.get("/contests", async (req, res) => {
+            const result = await contestCollection.find().toArray()
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
